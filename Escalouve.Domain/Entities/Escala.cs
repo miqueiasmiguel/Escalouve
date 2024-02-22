8c6c75757e57e2cadaa13ad6288c7581aa631308
Miquéias Filho <miqueias.s.filho@gmail.com>
2024-02-22 12:03:27 -0300

    Incluindo validações de domínio

--- Escalouve.Domain/Entities/Escala.cs
+++ Escalouve.Domain/Entities/Escala.cs
@@ -1,4 +1,6 @@
-﻿namespace Escalouve.Domain.Entities;
+﻿using Escalouve.Domain.Validation;
+
+namespace Escalouve.Domain.Entities;
 
 public sealed class Escala
 {
@@ -9,6 +11,20 @@ public sealed class Escala
 
     public Escala(DateTime data, IDictionary<Instrumento, int> layout)
     {
+        Validate(data, layout);
+    }
+
+    public void Update(DateTime data, IDictionary<Instrumento, int> layout)
+    {
+        Validate(data, layout);
+    }
+
+    private void Validate(DateTime data, IDictionary<Instrumento, int> layout)
+    {
+        DomainExceptionValidation.When(data < DateTime.MinValue, "Data inválida.");
+
+        DomainExceptionValidation.When(layout.Count < 1, "Layout Inválido. O layout está vazio.");
+
         Data = data;
         Layout = layout;
     }
