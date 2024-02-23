227513c66815d94c5e636d82bc556c14ab9b7258
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-02-23 09:38:57 -0300

    Instalando pacotes EF e criando classe de contexto

---
+++ Escalouve.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Escalouve.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escalouve.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Integrante> Integrantes { get; set; }
+        public DbSet<Instrumento> Instrumento { get; set; }
+        public DbSet<Escala> Escala { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
