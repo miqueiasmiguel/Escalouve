6c568dd25c8024f2ed0544097cf30e8c1beb64dd
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-02-27 20:13:00 -0300

    Criando mapeamentos entre entidade e dto

---
+++ Escalouve.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Escalouve.Application.Dtos;
+using Escalouve.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escalouve.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<IntegranteDto, Integrante>();
+            CreateMap<Integrante, IntegranteDto>();
+
+            CreateMap<InstrumentoDto, Instrumento>();
+            CreateMap<Instrumento, InstrumentoDto>();
+
+            CreateMap<EscalaDto, Escala>();
+            CreateMap<Escala, EscalaDto>();
+        }
+    }
+}
