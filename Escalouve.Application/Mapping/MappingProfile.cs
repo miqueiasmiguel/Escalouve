b8252f80880bf35ccc0565544efd0c8914103352
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-02-28 11:45:57 -0300

    Limpando imports inutilizados

--- Escalouve.Application/Mapping/MappingProfile.cs
+++ Escalouve.Application/Mapping/MappingProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using Escalouve.Application.Dtos;
 using Escalouve.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Escalouve.Application.Mapping
 {
--- Escalouve.Infra.Data/EntitiesConfiguration/EscalaConfiguration.cs
+++ Escalouve.Infra.Data/EntitiesConfiguration/EscalaConfiguration.cs
@@ -2,11 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Escalouve.Infra.Data.EntitiesConfiguration
 {
